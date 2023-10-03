13ba2b6f433ffeef7cd7b82d250d569d3b2b870d
regina <usmanova.re@yandex.ru>
2023-10-03 22:12:58 +0500

    Добавление кнопки, для перехода в новую активность, где будет возможность добавлять новые заметки floatingactionbutton 	Создаю новую активность, через интент в MainActivity запускаю ее, новые заметки добавляются в ArrayList, делаю проработку для того что бы не нужно было создавать новый объект MainActivity   (делаю static) и final (и что бы вдруг не присвоить новое значение) 	Оформление активности add_note 	Добавление работоспособности кнопки в активности add_note  (public void onClickSaveNote(View view)): -получаю значения из элементов; -метод .trim() -  убирает лишние пробелы; - получаю выбранную радиокнопку через ее id  .getCheckedRadioButtonId(); -теперь нахожу RadioButton по этому id и получаю приоритет; -у RadioButton получаем текст, что бы получить тип int вызываю метод .parseInt; -из полученных данных создается новая заметка; -и добавляется эта заметка в список заметок; -запускаю через Intent MainActivity.class
    	После новой заметки снова добавились первые элементы:
    	Так как в MainActivity в методе onCreate() не зависимо ни от чего добавляем новые элементы, поэтому делаю проверку if (notes.isEmpty()) -  если список заметок пуст, то в этом случае добавляем

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -4,30 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerViewNote;
-    private ArrayList<Note>notes=new ArrayList<>();
+    public static final ArrayList<Note>notes=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerViewNote =findViewById(R.id.recyclerViewNotes);
-        notes.add(new Note("задача_1", "подзадача_1","понедельник",4));
-        notes.add(new Note("задача_2", "подзадача_2","вторник",2));
-        notes.add(new Note("задача_3", "подзадача_3","среда",3));
-        notes.add(new Note("задача_4", "подзадача_4","четверг",2));
-        notes.add(new Note("задача_5", "подзадача_5","пятница",7));
-        notes.add(new Note("задача_6", "подзадача_6","понедельник",5));
-        notes.add(new Note("задача_7", "подзадача_7","суббота",2));
-        notes.add(new Note("задача_8", "подзадача_8","понедельник",1));
-        notes.add(new Note("задача_9", "подзадача_9","воскресенье",6));
+        if (notes.isEmpty()) {
+            notes.add(new Note("задача_1", "подзадача_1", "понедельник", 4));
+            notes.add(new Note("задача_2", "подзадача_2", "вторник", 2));
+            notes.add(new Note("задача_3", "подзадача_3", "среда", 3));
+            notes.add(new Note("задача_4", "подзадача_4", "четверг", 2));
+            notes.add(new Note("задача_5", "подзадача_5", "пятница", 7));
+            notes.add(new Note("задача_6", "подзадача_6", "понедельник", 5));
+            notes.add(new Note("задача_7", "подзадача_7", "суббота", 2));
+            notes.add(new Note("задача_8", "подзадача_8", "понедельник", 1));
+            notes.add(new Note("задача_9", "подзадача_9", "воскресенье", 6));
+        }
         NotesAdapter adapter = new NotesAdapter(notes);
         recyclerViewNote.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNote.setAdapter(adapter);
+    }
+
+    public void onClickAddNote(View view) {
+        Intent intent =new Intent(this,AddNoteActivity.class);
+        startActivity(intent);
     }
 }
