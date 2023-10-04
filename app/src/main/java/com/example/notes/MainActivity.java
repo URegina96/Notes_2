64401a24cb5422b14de6b79bef5e890e01097cd6
regina <usmanova.re@yandex.ru>
2023-10-05 00:09:47 +0500

    Внесла пару правок
    cursor.close(); - иначе могут быть шибки
    
    Данные закомментировала временно

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -30,17 +30,17 @@ public class MainActivity extends AppCompatActivity {
         recyclerViewNote = findViewById(R.id.recyclerViewNotes);
         dbHelper = new NotesDBHelper(this);
         SQLiteDatabase database = dbHelper.getWritableDatabase();
-        if (notes.isEmpty()) {
-            notes.add(new Note("задача_1", "подзадача_1", "понедельник", 4));
-            notes.add(new Note("задача_2", "подзадача_2", "вторник", 2));
-            notes.add(new Note("задача_3", "подзадача_3", "среда", 3));
-            notes.add(new Note("задача_4", "подзадача_4", "четверг", 2));
-            notes.add(new Note("задача_5", "подзадача_5", "пятница", 7));
-            notes.add(new Note("задача_6", "подзадача_6", "понедельник", 5));
-            notes.add(new Note("задача_7", "подзадача_7", "суббота", 2));
-            notes.add(new Note("задача_8", "подзадача_8", "понедельник", 1));
-            notes.add(new Note("задача_9", "подзадача_9", "воскресенье", 6));
-        }
+//        if (notes.isEmpty()) {
+//            notes.add(new Note("задача_1", "подзадача_1", "понедельник", 4));
+//            notes.add(new Note("задача_2", "подзадача_2", "вторник", 2));
+//            notes.add(new Note("задача_3", "подзадача_3", "среда", 3));
+//            notes.add(new Note("задача_4", "подзадача_4", "четверг", 2));
+//            notes.add(new Note("задача_5", "подзадача_5", "пятница", 7));
+//            notes.add(new Note("задача_6", "подзадача_6", "понедельник", 5));
+//            notes.add(new Note("задача_7", "подзадача_7", "суббота", 2));
+//            notes.add(new Note("задача_8", "подзадача_8", "понедельник", 1));
+//            notes.add(new Note("задача_9", "подзадача_9", "воскресенье", 6));
+//        }
         ArrayList<Note> notesFromDB = new ArrayList<>();
         Cursor cursor = database.query(NotesContract.NotesEntry.TABLE_NAME, null, null, null, null, null, null);
         while (cursor.moveToNext()) {
@@ -51,15 +51,16 @@ public class MainActivity extends AppCompatActivity {
             Note note=new Note(title,description,dayOfWeek,priority);
             notesFromDB.add(note);
         }
-        for (Note note : notes) {
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, note.getTitle());
-            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, note.getDescription());
-            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, note.getDayOfWeek());
-            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, note.getPriorty());
-            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
-        }
-        adapter = new NotesAdapter(notes);
+        cursor.close();
+//        for (Note note : notes) {
+//            ContentValues contentValues = new ContentValues();
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, note.getTitle());
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, note.getDescription());
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, note.getDayOfWeek());
+//            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, note.getPriorty());
+//            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
+//        }
+        adapter = new NotesAdapter(notesFromDB);
         recyclerViewNote.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNote.setAdapter(adapter);
         adapter.setOnNoteClickListner(new NotesAdapter.OnNoteClickListner() {
