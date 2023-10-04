08b01c65aadb53b1889ddf1b76d83d08a2d11169
regina <usmanova.re@yandex.ru>
2023-10-04 23:52:38 +0500

    Notes_2
    В MainActivity создаю объект помощника    и присвоила ему значение. Для получения с Helper базу данных – нужно записать данные в базу. Тогда нужно получить базу данных для записи, создаю ее – SQLiteDatabase
    
    Теперь начинаю заполнять таблицу (из массива Notes). Сделаю через цикл, так как возьму все данные из массива. В БД надо вставлять конкретные данные парами ключ-значение, так как в БД не вставляют просто объекты
    Объект класса ContentValues для того что бы вставить новую запись
    
    Теперь получаем данные из базы данных: читае данные из БД и сохраняем в новый ArrayList.
    Объект Cursor-вытаскивает все данные из БД;
    Для чего цикл while:
    Изначально cursor указывает на строку с индексом -1, вызываем метод  .moveToNext() , если там есть хоть какие-то записи, то он перейдет на строку с индексом 0 и вернет try и т.д. Прошлись по всем строкам – данным и затем создали новую заметку и поместили в ArrayList и так по циклу

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -6,11 +6,16 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
 
 import java.util.ArrayList;
 
@@ -18,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerViewNote;
     public static final ArrayList<Note> notes = new ArrayList<>();
     private NotesAdapter adapter;
+    private NotesDBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerViewNote = findViewById(R.id.recyclerViewNotes);
+        dbHelper = new NotesDBHelper(this);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
         if (notes.isEmpty()) {
             notes.add(new Note("задача_1", "подзадача_1", "понедельник", 4));
             notes.add(new Note("задача_2", "подзадача_2", "вторник", 2));
@@ -35,7 +43,25 @@ public class MainActivity extends AppCompatActivity {
             notes.add(new Note("задача_8", "подзадача_8", "понедельник", 1));
             notes.add(new Note("задача_9", "подзадача_9", "воскресенье", 6));
         }
-        NotesAdapter adapter = new NotesAdapter(notes);
+        ArrayList<Note> notesFromDB = new ArrayList<>();
+        Cursor cursor = database.query(NotesContract.NotesEntry.TABLE_NAME, null, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            String title = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_TITLE));
+            String description= cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DESCRIPTION));
+            String dayOfWeek = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK));
+            int priority = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_PRIORITY));
+            Note note=new Note(title,description,dayOfWeek,priority);
+            notesFromDB.add(note);
+        }
+        for (Note note : notes) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, note.getTitle());
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, note.getDescription());
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, note.getDayOfWeek());
+            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, note.getPriorty());
+            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
+        }
+        NotesAdapter adapter = new NotesAdapter(notesFromDB);
         recyclerViewNote.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNote.setAdapter(adapter);
         adapter.setOnNoteClickListner(new NotesAdapter.OnNoteClickListner() {
